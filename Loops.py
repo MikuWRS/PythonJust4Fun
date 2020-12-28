d7ad7aacaeadc56965fb4f66b273e09552729233
MikuWRS <sebastian.holloway@usach.cl>
2020-12-28 17:14:11 -0300

    Loops en Python

---
+++ Loops.py
@@ -0,0 +1,40 @@
+rangers=["rojo","azul","amarillo"]
+
+for i in rangers:
+    print(i)
+
+x=[1,2,3,4,5]
+for j in range(5):
+    x[j]=input("Numero:")
+    print(j)
+
+print(x)
+
+i=0
+while i<6:
+    print(i+1)
+    i+=1
+
+a="Arandano"
+
+for i in a:
+    print(i)
+
+for i in x:
+    if i=="55":
+        break
+    print(i)
+#        range(inicio, termino, de tanto en tanto)
+for z in range(5,25,3):
+    print(z)
+else:
+    print("Termine")
+
+liga=["Diamante","Platino","Oro","Plata","Bronce"]
+numero=["I","II","III","IV"]
+
+for i in liga:
+    for j in numero:}
+        print(i,j)
+print(len(liga)*len(numero))
+  
